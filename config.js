4b72e8ee62fe3547e44cf3e8dbefbadb830ca9a5
unknown <khanhzoithanh@gmail.com>
2021-11-23 11:01:14 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Chào bé Sò của EM ! 🖐🖐🖐',
+    introDesc: `Em có điều này muốn hỏi Anh, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Em biết anh muốn lụm em,trịt trịt em, thịt em lắm đúng hông ? 😙',
+    desc: 'Anh không trả lời hoặc thoát ra, tức là Anh không muốn đó nha :> ',
+    btnYes: 'Không, Anh không muốn 💖💖💖',
+    btnNo: '💔 Muốn chứ, Anh muốn lắm ! 💔 ',
+    question:'Nói cho Em biết đi vì sao Anh Không muốn lụm em đi 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh đẹp yếu sinh lý em à, vì ciuuu anh bé tí tẹo. Xinloi Em 😊😊',
+    mess: 'Em biết mà, Không sao em vẫn yêu anh, đừng tự ti nữa nha ngiu. Không lụm cũng được em không bỏ anh đâu 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Về Nhà với em. 💬💬',
+    btnAccept: 'Dạ 💖',
+    messLink: 'https://www.facebook.com/messages/t/100034724270027/'
+}
